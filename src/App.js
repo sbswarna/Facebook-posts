c7e5ccf8607d1dd3dc2001830ae0381d9479d1ac
Shatabdi Biswas Swarna <79458082+sbswarna@users.noreply.github.com>
2022-01-18 22:45:09 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import Posts from "./components/posts.component";
+import React, { Component } from 'react';
+import Navbar from './components/navbar.component';
+import Posts from './components/posts.component';
 
-function App() {
-  return (
-    <>
-      <Posts />
-    </>
-  );
+class App extends Component {
+  state = { 
+    totalNumberOfLikes: 0,
+    totalNumberOfDislikes: 0
+  } 
+
+  increaseTotalLikes = () => {
+    this.setState({ totalNumberOfLikes: this.state.totalNumberOfLikes + 1 });
+  }
+
+  decreaseTotalLikes = () => {
+    this.setState({ totalNumberOfLikes: this.state.totalNumberOfLikes - 1 });
+  }
+
+  increaseTotalDislikes = () => {
+    this.setState({ totalNumberOfDislikes: this.state.totalNumberOfDislikes + 1 });
+  }
+
+  decreaseTotalDislikes = () => {
+    this.setState({ totalNumberOfDislikes: this.state.totalNumberOfDislikes - 1 });
+  }
+
+  render() { 
+    return (
+      <>
+        <Navbar
+          totalNumberOfLikes={this.state.totalNumberOfLikes}
+          totalNumberOfDislikes={this.state.totalNumberOfDislikes}
+        />
+        <Posts 
+          increaseTotalLikes={this.increaseTotalLikes}
+          decreaseTotalLikes={this.decreaseTotalLikes}
+          increaseTotalDislikes={this.increaseTotalDislikes}
+          decreaseTotalDislikes={this.decreaseTotalDislikes}
+        />
+      </>
+    );
+  }
 }
-
+ 
 export default App;
